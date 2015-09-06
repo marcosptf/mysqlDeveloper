04d54fbb6f86649585436cc674d8bc9869a84ce2
root <marcosptf@yahoo.com.br>
2015-09-06 00:21:51 -0300

    add sql code to git-repo

---
+++ MSSqlServer/scriptDropDB.sql
@@ -0,0 +1,33 @@
+USE [Production];
+GO
+drop table [dbo].[PIUsuarioBK];
+GO
+drop table [dbo].[SpeedyAFOBK];
+GO
+drop table [dbo].[SpeedyAFOExportadoBK];
+GO
+drop table [dbo].[SpeedyCriticaBK];
+GO
+drop table [dbo].[SpeedyCustomersBK];
+GO
+drop table [dbo].[SpeedyFilesBK];
+GO
+drop table [dbo].[SpeedyFiltroBK];
+GO
+drop table [dbo].[SpeedyJobBK];
+GO
+drop table [dbo].[SpeedyLogBK];
+GO
+drop table [dbo].[SpeedyLogEnvioBK];
+GO
+drop table [dbo].[SpeedyOrdersBK];
+GO
+drop table [dbo].[SpeedySERBK];
+GO
+drop table [dbo].[SpeedySVABK];
+GO
+drop table [dbo].[SpeedyTrialBK];
+GO
+drop table [dbo].[SpeedyVencimentosBK];
+GO
+drop table [dbo].[SpeedyEmailBK];
